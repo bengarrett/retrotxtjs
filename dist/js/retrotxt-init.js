19d95325c4430fdba2be8043d8eefb920b87d248
Ben Garrett <code.by.ben@gmail.com>
2025-01-02 19:32:55 +1000

    package update.

--- dist/js/retrotxt-init.js
+++ dist/js/retrotxt-init.js
@@ -1,2 +1,2 @@
-window.onload=function(){var t;!function(){try{new Function("(a = 0) => a")}catch(t){return!1}try{document.createElement("div").append("some text")}catch(t){return!1}return!0}()||null==document.getElementById("retrotxt-canvas")||((t=document.createElement("script")).src="/js/retrotxt.js",t.type="module",document.body.appendChild(t))};
-/*! retrotxt v2.0.1 (2024-06-11); © Ben Garrett - LGPL-3.0 */
+window.onload=function(){var t;(()=>{try{new Function("(a = 0) => a")}catch(t){return!1}try{document.createElement("div").append("some text")}catch(t){return!1}return!0})()&&null!=document.getElementById("retrotxt-canvas")&&((t=document.createElement("script")).src="/js/retrotxt.js",t.type="module",document.body.appendChild(t))};
+/*! retrotxt v2.0.2 (2025-01-02); © Ben Garrett - LGPL-3.0 */
